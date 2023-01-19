8c08fcb625fde4518e9e29c4d70c811a5b2c091b
band8266 <band8266@gmail.com>
2023-01-19 09:30:59 -0700

    Update DFRobot_MICS.cpp

--- DFRobot_MICS.cpp
+++ DFRobot_MICS.cpp
@@ -187,8 +187,10 @@ float DFRobot_MICS::getGasData(uint8_t type)
   getSensorData(oxData, redData, powerData);
   float RS_R0_RED_data = (float)(powerData[0] - redData[0]) / init_r0_red;
   float RS_R0_OX_data = (float)(powerData[0] - oxData[0]) / init_r0_ox;
-  Serial.println(RS_R0_OX_data);
-  Serial.println(RS_R0_RED_data);
+  Serial.println(powerData[0]);
+  Serial.println(oxData[0]);
+  Serial.println(redData[0]);
+  Serial.println(init_r0_ox);
   switch(type){
     case 1:
       Serial.println(type);
