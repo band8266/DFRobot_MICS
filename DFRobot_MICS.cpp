00b3e0255209b6cbc2fba89f4cc01b57ac2002e7
band8266 <band8266@gmail.com>
2023-01-19 10:34:50 -0700

    Update DFRobot_MICS.cpp

--- DFRobot_MICS.cpp
+++ DFRobot_MICS.cpp
@@ -224,81 +224,81 @@ float DFRobot_MICS::getGasData(uint8_t type)
 float DFRobot_MICS::getCarbonMonoxide(float data)
 {
   Serial.println(data);
-//  if(data > 0.425)
-//    return 0.0;
+  if(data > 0.425)
+    return 0.0;
   float co = (0.425 - data) / 0.000405;
-//  if(co > 1000.0)
-//    return 1000.0;
-//  if(co < 1.0) 
- //   return 0.0;
+  if(co > 1000.0)
+    return 1000.0;
+  if(co < 1.0) 
+    return 0.0;
   return co;
   Serial.println(data);
 }
 
 float DFRobot_MICS::getEthanol(float data)
 {
-//  Serial.println(data);
-//  if(data > 0.306)
-//    return 0.0;
+  Serial.println(data);
+  if(data > 0.306)
+    return 0.0;
   float ethanol = (0.306 - data) / 0.00057;
-//  if(ethanol < 10.0) 
-//    return 0.0;
-//  if(ethanol > 500.0) 
-//    return 500.0;
+  if(ethanol < 10.0) 
+    return 0.0;
+  if(ethanol > 500.0) 
+    return 500.0;
   return ethanol;
 
 }
 
 float DFRobot_MICS::getMethane(float data)
 {
-//  Serial.println(data);
-//  if(data > 0.786)
-//    return 0.0;
+  Serial.println(data);
+  if(data > 0.786)
+    return 0.0;
   float methane = (0.786 - data) / 0.000023;
-//  if(methane < 1000.0) methane = 0.0;
-//  if(methane > 25000.0) methane = 25000.0;
+  if(methane < 1000.0) methane = 0.0;
+  if(methane > 25000.0) methane = 25000.0;
   return methane;
 }
 
 float DFRobot_MICS::getNitrogenDioxide(float data)
 {
-//  Serial.println(data);
-//  if(data < 1.1) return 0;
+  Serial.println(data);
+  if(data < 1.1) return 0;
   float nitrogendioxide = (data - 0.045) / 6.13;
-//  Serial.println(nitrogendioxide);
-//  if(nitrogendioxide < 0.1)
-//    return 0.0;
-//  if(nitrogendioxide > 10.0)
-//    return 10.0;
+  Serial.println(nitrogendioxide);
+  if(nitrogendioxide < 0.1)
+    return 0.0;
+  if(nitrogendioxide > 10.0)
+    return 10.0;
   return nitrogendioxide;
 }
 
 float DFRobot_MICS::getHydrogen(float data)
 {
-//  Serial.println(data);
-//  if(data > 0.279)
- //   return 0.0;
+  Serial.println(data);
+  if(data > 0.279)
+    return 0.0;
   float hydrogen = (0.279 - data) / 0.00026;
-//  if(hydrogen < 1.0) 
- //   return 0.0;
-//  if(hydrogen > 1000.0) 
-//    return 1000.0;
+  if(hydrogen < 1.0) 
+    return 0.0;
+  if(hydrogen > 1000.0) 
+    return 1000.0;
   return hydrogen;
-//  Serial.println(data);
+  Serial.println(data);
 }
 
 float DFRobot_MICS::getAmmonia(float data)
 {
-//  Serial.println(data);
-//  if(data > 0.8)
-//    return 0.0;
+  Serial.println(data);
+  if(data > 0.8)
+    return 0.0;
   float ammonia = (0.8 - data) / 0.0015;
-//  if(ammonia < 1.0) 
- //   return 0.0;
-//  if(ammonia > 500.0) 
-//    return 500.0;
+  if(ammonia < 1.0) 
+    return 0.0;
+  if(ammonia > 500.0) 
+    return 500.0;
   return ammonia;
-//  Serial.println(data);
+  Serial.println(data);
 }
 
 bool DFRobot_MICS::existIsoButane(float data)
